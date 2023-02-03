a15fae443d115f9f2661644eae67fb62cd14a7fd
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-02-03 13:20:28 +0700

    get token

---
+++ gettoken.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+const fs = require("fs");
+const login = async (email, password) => {
+  try {
+    const res = await axios.post(
+      "https://discord.com/api/v9/auth/login",
+      {
+        email,
+        password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const token = await res.data.token;
+    const restkn = `${token}\n`;
+
+    fs.appendFile("outoken.txt", restkn, "utf8", (err) => {
+      if (err) throw err;
+      console.log("token telah tersimpan di outoken.txt!");
+    });
+    console.log(token);
+  } catch (error) {
+    throw error;
+  }
+};
+
+login("legionbaj@gmail.com", "thePriVat029");
