0b4de15dd50e89c33e999198ff764cc203d0404d
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-02-10 10:17:29 +0700

    app.js v 2.0.0

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const { send, check, delet } = require("./fetch");
+const DiscordDatabase = require("discord-cloud-database");
 const chalk = require("chalk");
 const moment = require("moment");
+const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
 
 const delay = (time) => {
@@ -26,57 +28,50 @@ const timeRed = () => {
 ++++++++++++++++++++++++++++++++++++++++++++++
   `);
   while (true) {
+    const modulesToCheck = ["discord-cloud-database", "moment", "chalk", "dotenv"];
+
+    for (const moduleName of modulesToCheck) {
+      const modulePath = path.join(__dirname, "node_modules", moduleName);
+
+      if (!fs.existsSync(modulePath)) {
+        console.error(`Error: ${moduleName} is not installed. Please run "npm install ${moduleName}" and try again.`);
+        process.exit(1);
+      }
+    }
     const mess = process.env.MESSAGE.split(",");
-    // const randomMsg = mess[Math.floor(mess.length * Math.random())];
+
     const Token = process.env.TOKEN.split(",");
 
     let i = 0;
     while (i < Token.length) {
       const token = Token[i].trim();
       i++;
-      
+
       const randomMsg = mess[Math.floor(mess.length * Math.random())];
+      const discordDatabase = new DiscordDatabase(token, { risqi: process.env.CHANNEL_ID });
 
-      const readSend = await send(process.env.CHANNEL_ID, token, randomMsg);
-
-      const readSendMsg = await readSend.json();
-
-      const userReadSend = () => {
-        if (readSend.status === 200) {
-          return chalk.yellow(`[${readSendMsg.author.username}#${readSendMsg.author.discriminator}]`);
+      try {
+        const readSend = await discordDatabase.insertOne(randomMsg, { name: "risqi" });
+        // console.log(readSend);
+        const readSendMsg = await discordDatabase.findOne(readSend.id, { name: "risqi" });
+        // console.log(readSendMsg);
+        const usr = chalk.yellow(`${readSendMsg.author.username}#${readSendMsg.author.discriminator}`);
+        console.log(`[ ${timeGreen()} ] [${usr}] Send Messege: ${readSendMsg.content}`);
+        const deletById = await discordDatabase.deleteMessageById(readSend.id, { name: "risqi" });
+        // console.log(deletById);
+        if (deletById === true) {
+          console.log(`[ ${timeGreen()} ] [${usr}] Success delete: ${chalk.yellow(readSend.id)}`);
         } else {
-          return chalk.red("[Token Failed]");
+          console.log(`[ ${timeRed()} ] [${usr}] Failed delete: ${chalk.yellow(readSend.id)}`);
         }
-      };
-      const usr = await userReadSend();
-
-      if (readSend.status === 200) {
-        console.log(`[ ${timeGreen()} ] ${usr} Send Messege: ${readSendMsg.content}`);
-      } else {
-        console.log(`[ ${timeRed()} ] ${usr} Please Check Token!!!`);
-      }
-
-      await delay(process.env.DELAY);
-      const readCheck = await check(process.env.CHANNEL_ID, token);
-      if (readCheck.status === 200) {
-        const msg = await readCheck.json();
-
-        if (msg.length === 0) {
-          console.log(`[ ${timeRed()} ] ${usr} No detect message!!!`);
-          break;
+        await delay(process.env.DELAY);
+      } catch (error) {
+        if (error.error.discordError === true) {
+          const usr = chalk.red("Token Failed");
+          console.log(`[ ${timeRed()} ] [${usr}] Please Check Token!!!`);
         } else {
-          await delay(process.env.DELAY_DELETE);
-
-          const msgId = msg[0].id;
-          const readDelet = await delet(process.env.CHANNEL_ID, msgId, token);
-          if (readDelet.status === 204) {
-            console.log(`[ ${timeGreen()} ] ${usr} Message sent with ID ${chalk.yellow(msgId)} deleted successfully`);
-          } else {
-            console.log(`[ ${timeRed()} ] ${usr} Failed to delete message with ID ${chalk.yellow(msgId)}: ${chalk.yellow(readDelet.status)}`);
-          }
+          console.error(error);
         }
-      } else {
-        console.log(`[ ${timeRed()} ] ${usr} Failed to get messages on channel: ${chalk.yellow(readCheck.status)}`);
         await delay(process.env.DELAY);
       }
     }
