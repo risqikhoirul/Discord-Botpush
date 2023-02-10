73a1a012bbce826d49045629e29a897504c23eb9
M Khoirul Risqi <82764602+risqikhoirul@users.noreply.github.com>
2023-02-10 10:28:00 +0700

    Update token.js

--- token.js
+++ token.js
@@ -1,6 +1,7 @@
 const DiscordDatabase = require("discord-cloud-database");
 const discordDatabase = new DiscordDatabase();
 const fs = require("fs");
+const path = require("path");
 /**
  * M Khoirul Risqi
  * https://github.com/risqikhoirul
@@ -8,6 +9,16 @@ const fs = require("fs");
  */
 
 (async () => {
+    const modulesToCheck = ["discord-cloud-database"];
+
+    for (const moduleName of modulesToCheck) {
+      const modulePath = path.join(__dirname, "node_modules", moduleName);
+
+      if (!fs.existsSync(modulePath)) {
+        console.error(`Error: ${moduleName} is not installed. Please run "npm install ${moduleName}" and try again.`);
+        process.exit(1);
+      }
+    }
   const email = process.argv[2];
   const password = process.argv[3];
   try {
